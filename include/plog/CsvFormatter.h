154fbb534e2d8858c9303ebe6189d5c87a4aed88
Sergey Podobry <sergey.podobry@gmail.com>
2014-12-01 23:04:42 +0200

    Added heading zeroes for date and time.

--- include/plog/CsvFormatter.h
+++ include/plog/CsvFormatter.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <iomanip>
 
 namespace plog
 {
@@ -16,8 +17,8 @@ namespace plog
             util::localtime_s(&t, &entry.m_time.time);
 
             std::stringstream ss;
-            ss << t.tm_year + 1900 << "/" << t.tm_mon << "/" << t.tm_mday << ";";
-            ss << t.tm_hour << ":" << t.tm_min << ":" << t.tm_sec << "." << entry.m_time.millitm << ";";
+            ss << t.tm_year + 1900 << "/" << std::setfill('0') << std::setw(2) << t.tm_mon << "/" << std::setfill('0') << std::setw(2) << t.tm_mday << ";";
+            ss << std::setfill('0') << std::setw(2) << t.tm_hour << ":" << std::setfill('0') << std::setw(2) << t.tm_min << ":" << std::setfill('0') << std::setw(2) << t.tm_sec << "." << std::setfill('0') << std::setw(3) << entry.m_time.millitm << ";";
             ss << getLevelName(entry.m_severity) << ";";
             ss << entry.m_tid << ";";
             ss << entry.m_object << ";";
