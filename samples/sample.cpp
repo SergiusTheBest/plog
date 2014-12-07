694b427c8c719305c0880d18c336ed142ab6fc51
Sergey Podobry <sergey.podobry@stellaritysoftware.com>
2014-12-08 01:24:39 +0200

    Added a sample.

---
+++ samples/sample.cpp
@@ -0,0 +1,38 @@
+﻿#include <plog/Log.h>
+
+class Object
+{
+public:
+    Object()
+    {
+        LOG_DEBUG;
+    }
+
+    ~Object()
+    {
+        LOG_DEBUG;
+    }
+};
+
+int main()
+{
+    plog::init_csv("log.csv", plog::debug);
+
+    // Log levels
+    LOG_FATAL << "fatal";
+    LOG_ERROR << "error";
+    LOG_INFO << "info";
+    LOG_WARNING << "warning";
+    LOG_DEBUG << "debug";
+
+    // Multiline
+    LOG_INFO << "This\nis\na\nmultiline!";
+
+    // Quotes
+    LOG_INFO << "This is a message with \"quotes\"!";
+
+    // Log in a class (capture this pointer, c++ function names)
+    Object obj;
+
+    return 0;
+}
