ac06a0d2ab700da0a8c1c67c0fe18747c8fd04ee
Sergey Podobry <sergey.podobry@gmail.com>
2014-12-11 00:18:57 +0200

    Improved text formatter.

--- include/plog/TxtFormatter.h
+++ include/plog/TxtFormatter.h
@@ -6,6 +6,11 @@ namespace plog
     class TxtFormatter
     {
     public:
+        static std::string header()
+        {
+            return std::string();
+        }
+
         static std::string format(const Entry& entry)
         {
             tm t;
@@ -14,7 +19,7 @@ namespace plog
             std::stringstream ss;
             ss << t.tm_year + 1900 << "-" << std::setfill('0') << std::setw(2) << t.tm_mon << "-" << std::setfill('0') << std::setw(2) << t.tm_mday << " ";
             ss << std::setfill('0') << std::setw(2) << t.tm_hour << ":" << std::setfill('0') << std::setw(2) << t.tm_min << ":" << std::setfill('0') << std::setw(2) << t.tm_sec << "." << std::setfill('0') << std::setw(3) << entry.m_time.millitm << " ";
-            ss << getLevelName(entry.m_severity) << " ";
+            ss << std::setfill(' ') << std::setw(5) << std::left << getLevelName(entry.m_severity) << " ";
             ss << "[" << entry.m_tid << "] ";
             ss << "[" << entry.m_func << "@" << entry.m_line << "] ";
             ss << entry.m_stream.str() << "\n";
