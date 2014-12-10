d1a9dbfcec197fa61efe7087818a109188d97e91
Sergey Podobry <sergey.podobry@gmail.com>
2014-12-11 00:19:28 +0200

    Implemented universal init: csv and txt.

--- include/plog/Init.h
+++ include/plog/Init.h
@@ -1,10 +1,23 @@
 #pragma once
 #include <plog/Logger.h>
 #include <plog/CsvFormatter.h>
+#include <plog/TxtFormatter.h>
 #include <plog/FileAppender.h>
 
 namespace plog
 {
+    namespace
+    {
+        bool isCsv(const char* fileName)
+        {            
+            const char kCsvExt[] = ".csv";
+            const size_t kCsvExtLength = sizeof(kCsvExt) - 1;
+
+            size_t len = ::strlen(fileName);
+            return len >= kCsvExtLength && 0 == ::strcmp(&fileName[len - kCsvExtLength], kCsvExt);
+        }
+    }
+    
     inline void init_csv(const char* fileName, Level maxSeverity)
     {
         static FileAppender<CsvFormatter> fileAppender(fileName, maxSeverity);
@@ -17,15 +30,35 @@ namespace plog
     {
         init_csv(util::toString(fileName).c_str(), maxSeverity);
     }
+
+    inline void init_txt(const char* fileName, Level maxSeverity)
+    {
+        static FileAppender<TxtFormatter> fileAppender(fileName, maxSeverity);
+        static Logger logger;
+
+        logger.addAppender(&fileAppender);
+    }
+
+    inline void init_txt(const wchar_t* fileName, Level maxSeverity)
+    {
+        init_txt(util::toString(fileName).c_str(), maxSeverity);
+    }
     
     inline void init(const char* fileName, Level maxSeverity)
     {
-        init_csv(fileName, maxSeverity);
+        if (isCsv(fileName))
+        {
+            init_csv(fileName, maxSeverity);
+        }
+        else
+        {
+            init_txt(fileName, maxSeverity);
+        }
     }
 
     inline void init(const wchar_t* fileName, Level maxSeverity)
     {
-        init_csv(fileName, maxSeverity);
+        init(util::toString(fileName).c_str(), maxSeverity);
     }
 
     // For backward compatibility
