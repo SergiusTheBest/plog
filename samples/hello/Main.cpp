3f7b25abff38927ffcd3deb2629bd73ee8e5a606
Sergey Podobry <sergey.podobry@gmail.com>
2015-03-09 21:54:33 +0200

    Added hello log sample.

---
+++ samples/hello/Main.cpp
@@ -0,0 +1,12 @@
+﻿#include <plog/Log.h>
+
+int main()
+{
+    plog::init("hello-log.csv", plog::debug);
+
+    LOGD << "Hello log!";
+    LOG_DEBUG << "Hello log!";
+    LOG(plog::debug) << "Hello log!";
+
+    return 0;
+}
