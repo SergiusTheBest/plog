c6fede611f362d936a72259b5104df2ad43dbac2
Sergey Podobry <sergey.podobry@gmail.com>
2015-03-14 19:36:36 +0200

    Added MultiInstance sample.

---
+++ samples/MultiInstance/Main.cpp
@@ -0,0 +1,22 @@
+﻿#include <plog/Log.h>
+
+enum
+{
+    SecondLog = 1
+};
+
+int main()
+{
+    plog::init("MultiInstance-default.txt", plog::debug);
+    plog::init<SecondLog>("MultiInstance-second.txt", plog::debug);
+
+    LOGD << "Hello default log!";
+    LOG_DEBUG << "Hello default log!";
+    LOG(plog::debug) << "Hello default log!";
+
+    LOGD_(SecondLog) << "Hello second log!";
+    LOG_DEBUG_(SecondLog) << "Hello second log!";
+    LOG_(SecondLog, plog::debug) << "Hello second log!";
+
+    return 0;
+}
