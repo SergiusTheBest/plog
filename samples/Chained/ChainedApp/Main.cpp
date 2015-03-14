c93132d558a02bded3b016910998eb5aa4f089a9
Sergey Podobry <sergey.podobry@gmail.com>
2015-03-14 22:08:01 +0200

    Added Chained sample.

---
+++ samples/Chained/ChainedApp/Main.cpp
@@ -0,0 +1,16 @@
+﻿#include <plog/Log.h>
+
+extern "C" void init(plog::Severity severity, plog::IAppender* appender);
+extern "C" void foo();
+
+int main()
+{
+    plog::init("ChainedApp.txt", plog::debug);
+
+    LOGD << "Hello from app!";
+
+    init(plog::debug, &plog::get());
+    foo();
+
+    return 0;
+}
---
+++ samples/Chained/ChainedLib/Main.cpp
@@ -0,0 +1,11 @@
+﻿#include <plog/Log.h>
+
+extern "C" void init(plog::Severity severity, plog::IAppender* appender)
+{
+    plog::init(severity).addAppender(appender);
+}
+
+extern "C" void foo()
+{
+    LOGI << "Hello from shared lib!";
+}
