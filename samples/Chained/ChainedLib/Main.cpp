c52ba3fe6204db4496c437e25a4fc202500d3c7c
Sergey Podobry <sergey.podobry@gmail.com>
2015-03-15 18:38:23 +0200

    Fixed shared object default visibility.

--- samples/Chained/ChainedLib/Main.cpp
+++ samples/Chained/ChainedLib/Main.cpp
@@ -1,11 +1,17 @@
 ﻿#include <plog/Log.h>
 
-extern "C" void initialize(plog::Severity severity, plog::IAppender* appender)
+#if defined _MSC_VER || defined __CYGWIN__
+#   define EXPORT __declspec(dllexport)
+#else
+#   define EXPORT __attribute__ ((visibility ("default")))
+#endif
+
+extern "C" void EXPORT initialize(plog::Severity severity, plog::IAppender* appender)
 {
     plog::init(severity, appender);
 }
 
-extern "C" void foo()
+extern "C" void EXPORT foo()
 {
     LOGI << "Hello from shared lib!";
 }
