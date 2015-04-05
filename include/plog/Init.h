488d4dbc929b1b231b325aba33f907775c8af72a
Sergey Podobry <sergey.podobry@gmail.com>
2015-04-05 13:00:26 +0300

    Removed FileAppender as RollingFileAppender supersedes it.

--- include/plog/Init.h
+++ include/plog/Init.h
@@ -3,7 +3,6 @@
 #include <plog/Logger.h>
 #include <plog/Formatters/CsvFormatter.h>
 #include <plog/Formatters/TxtFormatter.h>
-#include <plog/Appenders/FileAppender.h>
 #include <plog/Appenders/RollingFileAppender.h>
 
 namespace plog
@@ -36,47 +35,28 @@ namespace plog
     }
 
     //////////////////////////////////////////////////////////////////////////
-    // File/RollingFile with any Formatter
+    // RollingFileAppender with any Formatter
 
-    template<int instance, class Formatter>
-    inline Logger<instance>& init(Severity maxSeverity, const char* fileName)
-    {
-        static FileAppender<Formatter> fileAppender(fileName);
-        return init<instance>(maxSeverity, &fileAppender);
-    }
-
-    template<int instance, class Formatter>
-    inline Logger<instance>& init(Severity maxSeverity, const char* fileName, size_t maxFileSize, int maxFiles)
+    template<class Formatter, int instance>
+    inline Logger<instance>& init(Severity maxSeverity, const char* fileName, size_t maxFileSize = 0, int maxFiles = 0)
     {
         static RollingFileAppender<Formatter> rollingFileAppender(fileName, maxFileSize, maxFiles);
         return init<instance>(maxSeverity, &rollingFileAppender);
     }
 
-    //////////////////////////////////////////////////////////////////////////
-    // File/RollingFile with CSV
-
-    template<int instance>
-    inline Logger<instance>& initCsv(Severity maxSeverity, const char* fileName, size_t maxFileSize = 0, int maxFiles = 0)
+    template<class Formatter>
+    inline Logger<0>& init(Severity maxSeverity, const char* fileName, size_t maxFileSize = 0, int maxFiles = 0)
     {
-        return maxFileSize ? init<instance, CsvFormatter>(maxSeverity, fileName, maxFileSize, maxFiles) : init<instance, CsvFormatter>(maxSeverity, fileName);
+        return init<Formatter, 0>(maxSeverity, fileName, maxFileSize, maxFiles);
     }
 
     //////////////////////////////////////////////////////////////////////////
-    // File/RollingFile with TXT
-
-    template<int instance>
-    inline Logger<instance>& initTxt(Severity maxSeverity, const char* fileName, size_t maxFileSize = 0, int maxFiles = 0)
-    {
-        return maxFileSize ? init<instance, TxtFormatter>(maxSeverity, fileName, maxFileSize, maxFiles) : init<instance, TxtFormatter>(maxSeverity, fileName);
-    }
-    
-    //////////////////////////////////////////////////////////////////////////
-    // File/RollingFile with TXT/CSV chosen by file extension
+    // RollingFileAppender with TXT/CSV chosen by file extension
 
     template<int instance>
     inline Logger<instance>& init(Severity maxSeverity, const char* fileName, size_t maxFileSize = 0, int maxFiles = 0)
     {
-        return isCsv(fileName) ? initCsv<instance>(maxSeverity, fileName, maxFileSize, maxFiles) : initTxt<instance>(maxSeverity, fileName, maxFileSize, maxFiles);
+        return isCsv(fileName) ? init<CsvFormatter, instance>(maxSeverity, fileName, maxFileSize, maxFiles) : init<TxtFormatter, instance>(maxSeverity, fileName, maxFileSize, maxFiles);
     }
 
     inline Logger<0>& init(Severity maxSeverity, const char* fileName, size_t maxFileSize = 0, int maxFiles = 0)
@@ -88,16 +68,16 @@ namespace plog
     // WCHAR variants for Windows
 
 #ifdef _WIN32
-    template<int instance>
-    inline Logger<instance>& initCsv(Severity maxSeverity, const wchar_t* fileName, size_t maxFileSize = 0, int maxFiles = 0)
+    template<class Formatter, int instance>
+    inline Logger<instance>& init(Severity maxSeverity, const wchar_t* fileName, size_t maxFileSize = 0, int maxFiles = 0)
     {
-        return init<instance, CsvFormatter>(maxSeverity, util::toString(fileName).c_str(), maxFileSize, maxFiles);
+        return init<Formatter, instance>(maxSeverity, util::toString(fileName).c_str(), maxFileSize, maxFiles);
     }
 
-    template<int instance>
-    inline Logger<instance>& initTxt(Severity maxSeverity, const wchar_t* fileName, size_t maxFileSize = 0, int maxFiles = 0)
+    template<class Formatter>
+    inline Logger<0>& init(Severity maxSeverity, const wchar_t* fileName, size_t maxFileSize = 0, int maxFiles = 0)
     {
-        return init<instance, TxtFormatter>(maxSeverity, util::toString(fileName).c_str(), maxFileSize, maxFiles);
+        return init<Formatter, 0>(maxSeverity, fileName, maxFileSize, maxFiles);
     }
 
     template<int instance>
@@ -108,7 +88,7 @@ namespace plog
 
     inline Logger<0>& init(Severity maxSeverity, const wchar_t* fileName, size_t maxFileSize = 0, int maxFiles = 0)
     {
-        return init<0>(maxSeverity, util::toString(fileName).c_str(), maxFileSize, maxFiles);
+        return init<0>(maxSeverity, fileName, maxFileSize, maxFiles);
     }
 #endif
 }
