9cda6d0516f41c00361b12d1a5916606e9b13a13
Victor Istomin <victor.send.mail.here@gmail.com>
2015-05-27 02:05:26 +0300

    Execution concurrency improved

--- include/plog/Appenders/RollingFileAppender.h
+++ include/plog/Appenders/RollingFileAppender.h
@@ -20,6 +20,7 @@ namespace plog
 
         virtual void write(const Record& record)
         {
+			const std::string line = Converter::convert(Formatter::format(record));
             util::MutexLock lock(m_mutex);
 
             if (m_firstWrite)
@@ -32,7 +33,7 @@ namespace plog
                 rollLogFiles();
             }
 
-            int bytesWritten = m_file.write(Converter::convert(Formatter::format(record)));
+            int bytesWritten = m_file.write(line);
 
             if (bytesWritten > 0)
             {
