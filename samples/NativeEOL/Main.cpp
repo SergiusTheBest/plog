00df7df1004f5495e73a1f635c43f74ef7180193
Sergey Podobry <sergey.podobry@gmail.com>
2017-05-02 17:57:03 +0300

    Add NativeEOLConverter.

---
+++ samples/NativeEOL/Main.cpp
@@ -0,0 +1,21 @@
+﻿//
+// NativeEOL - shows how to use NativeEOLConverter.
+//
+
+#include <plog/Log.h>
+#include <plog/Converters/NativeEOLConverter.h>
+
+int main()
+{
+    // NativeEOLConverter will use <CRLF> on Windows and <LF> on everything else as line endings. As a template parameter it accepts another converter (by default UTF8Converter).
+    static plog::RollingFileAppender<plog::TxtFormatter, plog::NativeEOLConverter<> > fileAppender("NativeEOL.log", 8000, 3); // Create an appender with NativeEOLConverter.
+    plog::init(plog::debug, &fileAppender); // Initialize the logger.
+
+    // Write some data.
+    for (int i = 0; i < 100; ++i)
+    {
+        LOGI << "i: " << i;
+    }
+
+    return 0;
+}
