bc2992f9db5d976ae28ca21956bc9ced290c63e0
Sergey Podobry <sergey.podobry@gmail.com>
2018-02-15 16:17:09 +0200

    Convert tabs -> spaces.

--- include/plog/Util.h
+++ include/plog/Util.h
@@ -97,9 +97,9 @@ namespace plog
 #elif defined(__linux__)
             return static_cast<unsigned int>(::syscall(__NR_gettid));
 #elif defined(__FreeBSD__)
-			long tid;
-			syscall(SYS_thr_self, &tid);
-			return static_cast<unsigned int>(tid);
+            long tid;
+            syscall(SYS_thr_self, &tid);
+            return static_cast<unsigned int>(tid);
 #elif defined(__APPLE__)
             uint64_t tid64;
             pthread_threadid_np(NULL, &tid64);
