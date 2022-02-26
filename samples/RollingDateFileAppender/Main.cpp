d900a3e16f5c136d3318a6f9309ab5c8a903d34a
caesar <caesar@kekxv.com>
2022-02-26 23:46:35 +0800

    add RollingDateFileAppender.h rolling file appender by date

---
+++ samples/RollingDateFileAppender/Main.cpp
@@ -0,0 +1,25 @@
+//
+// Hello - a minimal introduction sample, shows the basic 3 steps to start using plog.
+//
+
+#include <plog/Log.h> // Step1: include the header.
+#include <plog/Initializers/RollingFileInitializer.h>
+#include <plog/Appenders/RollingDateFileAppender.h>
+#include <thread>
+
+int main() {
+    using plog::TxtFormatter;
+    static plog::RollingDateFileAppender<TxtFormatter> dateFileAppender("Hello_date.%Y-%m-%d.log");
+
+
+    plog::init(plog::debug, &dateFileAppender); // Step2: initialize the logger.
+
+    // Step3: write log messages using a special macro. There are several log macros, use the macro you liked the most.
+    for (int i = 0; i < 100; i++) {
+        PLOGD << "Hello log!"; // short macro
+        PLOG_DEBUG << "Hello log!"; // long macro
+        PLOG(plog::debug) << "Hello log!"; // function-style macro
+        std::this_thread::sleep_for(std::chrono::seconds(2));
+    }
+    return 0;
+}
