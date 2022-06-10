3306953bcea57d868e2341642694b0f0411e1b45
Sergey Podobry <sergey.podobry@gmail.com>
2022-06-10 17:29:51 +0300

    Fix building of samples due to incosistent c++ standard

--- samples/CXX11/Main.cpp
+++ samples/CXX11/Main.cpp
@@ -14,13 +14,19 @@ int main()
 {
     plog::init<plog::TxtFormatter>(plog::debug, plog::streamStdOut); // Initialize logging
 
-    std::unordered_map<std::string, int> unorderedMap = { {"red", 1}, {"green", 2}, {"blue", 4} };
+    std::unordered_map<std::string, int> unorderedMap;
+    unorderedMap["red"] = 1;
+    unorderedMap["green"] = 2;
+    unorderedMap["blue"] = 4;
     PLOG_INFO << unorderedMap;
 
-    std::unordered_set<std::string> unorderedSet = { "red", "green", "blue" };
+    std::unordered_set<std::string> unorderedSet;
+    unorderedSet.insert("red");
+    unorderedSet.insert("green");
+    unorderedSet.insert("blue");
     PLOG_INFO << unorderedSet;
 
-    std::array<int, 4> array = {1, 2, 3, 4};
+    std::array<int, 4> array = {{1, 2, 3, 4}};
     PLOG_INFO << array;
 
     return 0;
