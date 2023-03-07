c12efd05a3c7970de186a18a9a2e79e3cc07ceef
Koutaro Mukai <mukai.k1011k@outlook.jp>
2023-03-07 23:26:41 +0900

    Make sample working

--- samples/Arduino/src/main.cpp
+++ samples/Arduino/src/main.cpp
@@ -1,8 +1,22 @@
 #include <Arduino.h>
+
 #include <plog/Log.h>
+#include <plog/Init.h>
+#include <plog/Formatters/TxtFormatter.h>
+#include <plog/Appenders/ArduinoAppender.h>
+
+static plog::ArduinoAppender<plog::TxtFormatter> arduinoAppender(Serial);
 
 void setup()
 {
+    plog::init(plog::verbose, &arduinoAppender);
+
+    PLOG_VERBOSE << "verbose";
+    PLOG_DEBUG << "debug";
+    PLOG_INFO << "info";
+    PLOG_WARNING << "warning";
+    PLOG_ERROR << "error";
+    PLOG_FATAL << "fatal";
 }
 
 void loop()
